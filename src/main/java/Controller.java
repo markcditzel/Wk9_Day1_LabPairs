0bfcb5febf187e5d4c57315d38dcb80025b452ad
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-20 21:58:44 +0100

    Established Spark for provide random name

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,22 +1,26 @@
+import models.Cohort;
 import models.Student;
+import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.get;
 
 public class Controller {
     public static void main(String[] args) {
 
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
-//
-//        get("/random", (req, res) -> {
-//            Student student = new Student();
-//        }
-//
-//    )
+
+        get("/random", (req, res) -> {
+            Cohort cohort = new Cohort("E23");
+            String result = cohort.getRandomStudent().getName();
+            Map<String, Object> model = new HashMap<>();
+            model.put("result", result);
+            return new ModelAndView(model, "resultRandomStudent.vtl");
+        }, velocityTemplateEngine);
     }
-
-
-
 
 }
